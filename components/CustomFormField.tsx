54f90be9200f219d95a197d2620a617e77db9c61
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-06 09:27:35 -0300

    creating patient form

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Control } from "react-hook-form"
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
+import { Input } from "./ui/input"
+import { FormFieldType } from "./ui/forms/PatientForm"
+
+interface CustomProps{
+  control:Control<any>
+  fieldType:FormFieldType
+}
+
+
+const CustomFormField = ({control,fieldType}:CustomProps) => {
+  return (
+    <FormField
+      control={control}
+      name="username"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>
+            This is your public display name.
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default CustomFormField
