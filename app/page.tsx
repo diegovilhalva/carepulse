8d292f10c964475049dcd540cc7399f55c022498
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-09 13:55:04 -0300

    admin passkey verification implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 
 import PatientForm from "@/components/forms/PatientForm";
+import Passkey from "@/components/Passkey";
 import Image from "next/image";
 import Link from "next/link";
 
 
-export default function Home() {
+export default function Home({searchParams}:SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true'
   return (
     <div className="flex h-screen max-h-screen ">
+      {isAdmin && (
+        <Passkey />
+      )}
       <section className="remove-scrollbar container my-auto ">
         <div className="sub-container max-w-[496px]">
           <Image src="/assets/icons/logo-full.svg" height={1000} width={1000} alt="patient" className="mb-12 h-10 w-fit" />
@@ -14,13 +19,13 @@ export default function Home() {
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">&copy; 2025 CarePulse</p>
             <Link href="/?admin=true" className="text-green-500">
-            Admin
+              Admin
             </Link>
           </div>
         </div>
       </section>
-      <Image 
-      src="/assets/images/onboarding-img.png" height={1000} width={1000} alt="patient" className="side-img max-w-[50%]" />
+      <Image
+        src="/assets/images/onboarding-img.png" height={1000} width={1000} alt="patient" className="side-img max-w-[50%]" />
     </div>
   );
 }
