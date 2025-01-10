94db08240c9a918dcf518b84c9ac33a53b298f1d
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-10 14:14:20 -0300

    sms notifications created

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -119,10 +119,25 @@ export const getAppointment = async (appointmentId: string) => {
   }
 };
 
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.error("An error occurred while sending sms:", error);
+  }
+};
+
 export const updateAppointment = async ({
   appointmentId,
   userId,
-  //timeZone,
+  timeZone,
   appointment,
   type,
 }: UpdateAppointmentParams) => {
@@ -137,8 +152,8 @@ export const updateAppointment = async ({
 
     if (!updatedAppointment) throw Error;
 
-    /*const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!, timeZone).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!, timeZone).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
-    await sendSMSNotification(userId, smsMessage);*/
+    const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!, timeZone).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!, timeZone).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
